f039a94f76525cee6c2fc604ed6fe190964ae4c7
Kushal Thakkar <kushal.thakkar@gmail.com>
2025-06-02 22:24:30 -0700

    add back pre-commit install

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -17,6 +17,7 @@ poetry install --with dev
 
 # Install the pre-commit hooks
 echo "Installing git hooks..."
+poetry run pre-commit install
 
 echo "Pre-commit hooks installed successfully!"
 echo "Now your code will be automatically checked and formatted when you commit."
